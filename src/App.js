9d8aae03475c7609e0a3cfb55a3dee58f4dec8a5
IbrahimSaeedPurdue <54752803+IbrahimSaeedPurdue@users.noreply.github.com>
2020-06-24 14:38:43 -0800

    mobileViewHide panel

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import SortingVisualizer from './containers/sortingVisualizer/SortingVisualizer';
+import Auxi from './hoc/Auxi';
+import './App.css';
 
 function App() {
   return (
-    <SortingVisualizer />
+    <Auxi>
+      <SortingVisualizer />
+      <div className="mobileView">
+        <div>
+          Mobile View Coming Soon :D
+        </div>
+      </div>
+    </Auxi>
+
   );
 }
 
--- src/Layout/UI/Toolbar/Toolbar.js
+++ src/Layout/UI/Toolbar/Toolbar.js
@@ -3,23 +3,23 @@ import NavigationItems from '../Navigation/NavigationItems/NavigationItems';
 import './Toolbar.scss';
 
 const toolbar = (props) => (
-  <nav className="Navbar">
-    <div className="Container">
-      <div className="NavbarHeader">
-        <button className="NavbarToggler">
-          <span></span>
-          <span></span>
-          <span></span>
-        </button>
-        <a href="/">Sorting <span>Visualizer</span></a>
+    <nav className="Navbar">
+      <div className="Container">
+        <div className="NavbarHeader">
+          <button className="NavbarToggler">
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+          <a href="/">Sorting <span>Visualizer</span></a>
 
+        </div>
+        <div className="NavbarMenu" id="open-navbar1">
+          {/* HERE */}
+          <NavigationItems currentAlgoHandler={props.currentAlgoHandler} />
+        </div>
       </div>
-      <div className="NavbarMenu" id="open-navbar1">
-        {/* HERE */}
-        <NavigationItems currentAlgoHandler={props.currentAlgoHandler} />
-      </div>
-    </div>
-  </nav>
+    </nav>
 );
 
 export default toolbar;
